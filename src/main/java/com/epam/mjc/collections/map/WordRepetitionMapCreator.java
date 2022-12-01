2ce0dce7565351d203928a4a011d09af72dbfc7b
Max <maxageev5@gmail.com>
2022-12-01 20:40:40 +0300

    1

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,28 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+
+        int count = 0;
+
+        Map<String, Integer> map = new HashMap<>();
+
+        String str = sentence.toLowerCase();
+
+        String[] array = str.split("\\W+");
+
+        for (String string : array) {
+            for (String string1 : array) {
+                if (string.equals(string1)) {
+                    count++;
+                }
+            }
+            map.put(string, count);
+            count = 0;
+        }
+        return map;
     }
 }
